366e18e070ec56a09734d0af35678ba1141800f4
pallasLi <ytli1987@163.com>
2017-04-17 10:03:50 +0800

    1

---
+++ objective-c/PallasliEdu/PallasliEdu/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PallasliEdu
+//
+//  Created by lyt1987 on 16/8/15.
+//  Copyright © 2016年 lyt1987. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ swift/bpmOA/BpmOA/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  BpmOA
+//
+//  Created by lyt1987 on 16/10/2.
+//  Copyright © 2016年 lyt1987. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func startGame(){
+        let myalert = UIAlertView()
+        myalert.title = "准备好了吗"
+        myalert.message = "准备好开始了吗？"
+        myalert.addButtonWithTitle("Ready, go!")
+        myalert.show()
+        let myStoryBoard = self.storyboard
+        let anotherView:UIViewController = myStoryBoard!.instantiateViewControllerWithIdentifier("home") as UIViewController
+        self.presentViewController(anotherView, animated: true, completion: nil)
+    }
+
+
+}
+
